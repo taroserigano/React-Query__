77cc8e6cff37653dbaf70e00ce0ed8aa35cf4d31
Taro Serigano <taroserigano@gmail.com>
2023-08-27 17:18:24 -0400

    Update axios-utils.js

--- react-query-demo/src/utils/axios-utils.js
+++ react-query-demo/src/utils/axios-utils.js
@@ -2,10 +2,14 @@ import axios from 'axios'
 
 const client = axios.create({ baseURL: 'http://localhost:4000' })
 
+// options take like URLs 
 export const request = ({ ...options }) => {
   client.defaults.headers.common.Authorization = `Bearer token`
 
+  // when successfully fetched 
   const onSuccess = response => response
+
+  // error case 
   const onError = error => {
     // optionaly catch errors and add additional logging here
     return error
