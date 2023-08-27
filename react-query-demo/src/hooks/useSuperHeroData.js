04d308e2d536dc661868c4650e65d87279d31dc2
Taro Serigano <taroserigano@gmail.com>
2023-08-27 17:22:22 -0400

    Update useSuperHeroData.js

--- react-query-demo/src/hooks/useSuperHeroData.js
+++ react-query-demo/src/hooks/useSuperHeroData.js
@@ -2,12 +2,16 @@ import { useQuery, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const fetchSuperHero = ({ queryKey }) => {
+
+  // [name, ID], so pick up the ID from the array 
   const heroId = queryKey[1]
   return axios.get(`http://localhost:4000/superheroes/${heroId}`)
 }
 
 export const useSuperHeroData = heroId => {
   const queryClient = useQueryClient()
+
+  // from "super-hero", find the ID 
   return useQuery(['super-hero', heroId], fetchSuperHero, {
     initialData: () => {
       const hero = queryClient
